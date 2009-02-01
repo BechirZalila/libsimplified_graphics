c11cf1eee21e472bd80a61cdc859eff790457fc6
Bechir Zalila <bechir.zalila@gmail.com>
2009-02-01 12:42:30 +0000

    * Documentation
    git-svn-id: https://svn.zalila.net/svn/ulysse/work/libsimplified_graphics@1125 643dc046-8b15-0410-bf49-ad7b499e6f5d

--- simple_test.c
+++ simple_test.c
@@ -14,25 +14,50 @@ int main (void) {
   char size [5], msg [50];
   char tmpl [] = "Hello There @ '%s pixels'";
 
+
+  /* Initialisation de l'environnement grahique avec les coordonnée
+     extrêmales. */
   Initialisation_Graphique (-100, -150.7, 400.5, 300);
 
+  /* Trace un trait entre les points (0, 0) et (50, 60) avec la
+     couleur 6 (Brown) */
   Trait (0.0, 0.0, 50, 60, 6);
 
+  /* Création d'une nouvelle couleur RGB (128, 128, 128) (Gris) et
+     placement de cette couleur à la position 77. */
   CreateColorRGB (77, 128, 128, 128);
-  Trait (10.0, 0.0, 60, 60, 77);
-  Croix (0.5, 150, 0);
 
+  /* Trace un trait entre les points (10, 0) et (60, 60) en utilisant
+     la nouvelle couleur, 77. */
+  Trait (10.0, 0.0, 60, 60, 77);
+
+  /* Affichage d'une crois au point (0.5, 150) en utilisant la couleur
+     noire (Black) */
+  Croix (0.5, 150, Black);
+
+  /* Affichage de 16 points avec les 16 premières couleurs */
   for (i = 0; i < 16; i++) {
     Point (100 + i * 10, 200.0, i);
   }
 
+  /* Affichange de 16 cercles avec les 16 premieres couleurs et de 16
+     autres disques avecs un ordre de couleurs inverse. */
   for (i = 0; i < 16; i++) {
     Cercle (-80.0 + i * 30, 230.0, 15, i);
     Disque (-80.0 + i * 30, 260.0, 15, i);
   }
 
-  DrawPoly (6, poly_1, 10);
-  FillPoly (6, poly_2, 10);
+  /* Traçage d'un polygone en vert clair */
+  
+  DrawPoly (6, poly_1, LightGreen);
+
+  /* Remplissage d'un second polygone légèrement plus petit que le
+     premier, avec la couleur verte */
+  FillPoly (6, poly_2, Green);
+
+  /* Traçage de 16 triangles avec les 16 premières couleurs et
+     remplissages de 16 autres triangles légèrement plus petis avec un
+     ordre de couleurs inverse. */
 
   for (i = 0; i < 16; i++) {
     triangle [0] = -90.0 + 30 * i;
@@ -51,11 +76,19 @@ int main (void) {
     FillTriangle (triangle, 15 - i);
   }
 
+  /* Traçage de 16 carrés avec les 16 premières couleurs et
+     remplissages de 16 rectangles inscrits dans les carrés avec un
+     ordre de couleurs inverse. */
+
   for (i = 0; i < 16; i++) {
     DrawRectangle (-90.0 + 30 * i, -120.0, -60.0 + 30 * i, -90.0, i);
     FillRectangle (-85.0 + 30 * i, -115.0, -65.0 + 30 * i, -90.0, 15 - i);
   }
 
+  /* Affichage de quelques textes avec des tailles et des couleurs
+     différentes. Traçages de rectangles ayant la même hauteur du
+     texte à chaque fois. */
+  
   Hauteur_Texte (8);
   itos (8, size);
   sprintf (msg, tmpl, size);
@@ -81,12 +114,16 @@ int main (void) {
   EcritXY (50, 0, msg, 4);
   DrawRectangle (50, 0, 150, 15, 0);
  
+  /* Attente de la touche ENTREE de l'utilisateur */
   Attente ();
 
+  /* Efface le contenu de la fenêtre */
   Efface ();
 
+  /* Une seconde attente */
   Attente ();
 
+  /* Destruction de la fenêtre graphique */
   Fin_Graphique ();
 
   return 0;
